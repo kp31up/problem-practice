a8b09590dd8fa441e00b3ee3ffbf46fd3ba4cc64
kp31up <91668583+kp31up@users.noreply.github.com>
2024-06-13 22:16:13 +0530

    Update CustomGlobalException.java

--- exceptions/CustomGlobalException.java
+++ exceptions/CustomGlobalException.java
@@ -1,4 +1,12 @@
 package exceptions;
 
-public class CustomGlobalException {
+public class CustomGlobalException extends Exception {
+
+    public CustomGlobalException(String message, Exception ex) {
+        super(message, ex.getCause());
+    }
+
+    public CustomGlobalException(String message) {
+        super(message);
+    }
 }
