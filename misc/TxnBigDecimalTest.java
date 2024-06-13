50d30c13a501beeea9dc9cddb9af4acb6f8f19f2
kp31up <upadhyaykapil31@yahoo.com>
2024-06-13 22:50:54 +0530

    TxnBigDecimalTest cases

--- misc/TxnBigDecimalTest.java
+++ misc/TxnBigDecimalTest.java
@@ -1,9 +1,16 @@
 package misc;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TxnBigDecimalTest {
-  Sales sales = new Sales();
+    TxnBigDecimal sales = new TxnBigDecimal();
     @Test
     void calcCommNegative() {
         assertThrows(IllegalArgumentException.class, () -> sales.calcCommOpt("-5000"));
