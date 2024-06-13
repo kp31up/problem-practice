764ff8d57ad9dd5b766595e735b5448c11b84a23
kp31up <91668583+kp31up@users.noreply.github.com>
2024-06-13 22:27:33 +0530

    Create Palindrome.java

---
+++ misc/Palindrome.java
@@ -0,0 +1,22 @@
+package problems.misc;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int original = sc.nextInt();
+        boolean isPalin = original == isPalindrome(original);
+        System.out.println("Is it a palindrome?  : " + isPalin);
+    }
+    public static int isPalindrome(int x) {
+        int pd = 0;
+        while(x > 0){
+            int digit = x%10;
+            pd = pd*10 + digit;
+            x = x/10;
+        }
+        return pd;
+    }
+}
