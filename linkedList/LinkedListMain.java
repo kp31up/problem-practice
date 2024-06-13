c5d74dbba7aba9f351ba0298684ff63542f76ef9
kp31up <91668583+kp31up@users.noreply.github.com>
2024-06-13 22:20:41 +0530

    Update LinkedListMain.java

--- linkedList/LinkedListMain.java
+++ linkedList/LinkedListMain.java
@@ -1,4 +1,36 @@
 package linkedList;
 
+import exceptions.CustomGlobalException;
+
+import java.util.logging.Logger;
+
 public class LinkedListMain {
+    static Logger log = Logger.getLogger(SinglyLinearLL.class.getName());
+
+    public static void main(String[] args) {
+        SinglyLinearLL ll = new SinglyLinearLL();
+        try {
+            ll.addFirst(3);
+            ll.addFirst(5);
+            ll.addFirst(8);
+            ll.addLast(1);
+            ll.displayList();
+
+            ll.addLast(2);
+            ll.addLast(1);
+            ll.displayList();
+
+            ll.removeKey(1);
+            System.out.print("List after removing key: ");
+            ll.displayList();
+
+            ll.removeLast();
+            ll.displayList();
+
+            ll.removeAtPos(2);
+            ll.displayList();
+        } catch (CustomGlobalException e) {
+            log.severe("Exception occurred: "+ e.getMessage());
+        }
+    }
 }
