be8bde14bdd3868d420e67d22bf6229a34c45f93
kp31up <upadhyaykapil31@yahoo.com>
2024-07-11 14:07:38 +0530

    ReverseArray by Recursion

--- ArrayProblems/ReverseArray.java
+++ ArrayProblems/ReverseArray.java
@@ -10,6 +10,10 @@ public class ReverseArray {
         printReverse(arr, n);
 
         System.out.println(Arrays.toString(getReversedBySwap(arr, n)));
+
+        int[] arrEven = {35, 1, 56, 36, 6, 1, 21, 13, 73, 2};
+        getReversedByRecursion(arrEven, 0, arrEven.length-1);
+        System.out.println(Arrays.toString(arrEven));
     }
 
     //Space = O(n), time = O(n)
@@ -33,4 +37,13 @@ public class ReverseArray {
     }
 
     //RECURSION: Space = O(1), time = O(n)
+    static void getReversedByRecursion(int[] arr, int l, int r) {
+        if (l == r || l > r) {
+            return;
+        }
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        getReversedByRecursion(arr, l+1, r-1);
+    }
 }
