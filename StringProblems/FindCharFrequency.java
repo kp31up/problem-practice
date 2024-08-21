7bce07aabc3ab6b72b6f164edf0a78d0da23e2d9
kp31up <upadhyaykapil31@yahoo.com>
2024-08-21 22:06:50 +0530

    Solution update

--- StringProblems/FindCharFrequency.java
+++ StringProblems/FindCharFrequency.java
@@ -6,15 +6,25 @@ public class FindCharFrequency {
 
         public static void main(String[] args) {
             String input = "abracadabra";
-            Scanner sc = new Scanner(System.in);
+            System.out.println("Select an alphabet from: " + input);
 
+            Scanner sc = new Scanner(System.in);
             char targetChar = sc.next().charAt(0);
 
+            //Solution 1
             long count = input.toLowerCase().chars()
                     .filter(ch -> ch == targetChar)
                     .count();
 
             System.out.println("The character '" + targetChar + "' is repeated " + count + " times in the string.");
+
+            //Solution 2
+            int cnt = 0;
+            for(char c: input.toCharArray()) {
+                if(c == targetChar)
+                    cnt++;
+            }
+            System.out.println("Count of alphabet = " + cnt);
         }
 
 }
