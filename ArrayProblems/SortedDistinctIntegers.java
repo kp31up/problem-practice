f4a724ea0a341b92f8cab7f10fd270578fd59f12
kp31up <upadhyaykapil31@yahoo.com>
2024-08-26 17:56:31 +0530

    map problem

--- ArrayProblems/SortedDistinctIntegers.java
+++ ArrayProblems/SortedDistinctIntegers.java
@@ -1,9 +1,6 @@
 package ArrayProblems;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SortedDistinctIntegers {
@@ -19,5 +16,26 @@ public class SortedDistinctIntegers {
             set.add(i);
         }
         System.out.println("Final list: " + set);
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("One", 1);
+        map.put("One three", 13);
+        map.put("One eight", 18);
+        map.put("two two", 22);
+
+        List<Integer> al = getEvenInt(map);
+
+        System.out.println("The even list: " + al);
+    }
+
+    // finding the no from map even and greater than ten
+    public static List<Integer> getEvenInt(Map<String, Integer> map) {
+        List<Integer> list = new ArrayList<>();
+        for (int n: map.values()) {
+            if (n > 10 && (n%2 == 0)) {
+                list.add(n);
+            }
+        }
+        return list;
     }
 }
