109219d7767f7f9b2cc9dab46360fee7e1d25db8
kp31up <upadhyaykapil31@yahoo.com>
2024-08-29 18:08:19 +0530

    update

--- concepts/SingletonEx.java
+++ concepts/SingletonEx.java
@@ -4,11 +4,11 @@ public class SingletonEx {
 
     private static SingletonEx instance;
 
-    public SingletonEx() {
+    private SingletonEx() {
 
     }
 
-    public static SingletonEx getInstance() {
+    synchronized public static SingletonEx getInstance() {
         if (instance == null)
             instance = new SingletonEx();
         return instance;
