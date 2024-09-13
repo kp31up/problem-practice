5ed2249c64a07d2e4cd164dee345edae1c05eb1d
kp31up <upadhyaykapil31@yahoo.com>
2024-09-13 16:05:12 +0530

    UncommonCharacters correction

--- StringProblems/UncommonCharacters.java
+++ StringProblems/UncommonCharacters.java
@@ -1,35 +1,45 @@
 package StringProblems;
 
 import java.util.HashSet;
+import java.util.TreeSet;
 
 //Find uncommon characters of the two strings. Given strings 'str1' and 'str2'. Return a string of characters which are
 // present in either of them but not both.The strings contain only lowercase characters and can contain duplicates.
 public class UncommonCharacters {
     public static void main(String[] args) {
-        String s1 = "Kohli";
-        String s2 = "Dhoni";
+        String s1 = "kohlik";
+        String s2 = "dhonid";
 
         System.out.println("Uncommon character string: "+ getUncommonChars(s1, s2));
     }
 
     private static String getUncommonChars(String s1, String s2) {
+
+        TreeSet<Character> tree= new TreeSet<>();
+
+        getDistinct(s1, s2, tree);
+
+        getDistinct(s2, s1, tree);
+
         StringBuilder sb = new StringBuilder();
+        for(char c: tree){
+            sb.append(c);
+        }
 
-        getDitinct(s1, s2, sb);
-
-        getDitinct(s2, s1, sb);
-
+        if (tree.isEmpty()){
+            return "-1";
+        }
         return sb.toString();
     }
 
-    private static void getDitinct(String s1, String s2, StringBuilder sb) {
+    private static void getDistinct(String s1, String s2, TreeSet<Character> tree) {
         HashSet<Character> set1 = new HashSet<>();
         for(char c : s1.toCharArray()){
             set1.add(c);
         }
         for (char c: s2.toCharArray()) {
             if (!set1.contains(c)){
-                sb.append(c);
+                tree.add(c);
             }
         }
     }
