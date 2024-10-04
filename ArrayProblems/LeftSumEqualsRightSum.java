b173376e22326672056698f107de8bec59e9ad24
kp31up <upadhyaykapil31@yahoo.com>
2024-10-04 17:39:35 +0530

    LeftSumEqualsRightSum

---
+++ ArrayProblems/LeftSumEqualsRightSum.java
@@ -0,0 +1,36 @@
+package ArrayProblems;
+
+import java.util.Arrays;
+import java.util.List;
+
+// From a given list of integers find the index at which the sum of left elements = sum of right elements.
+public class LeftSumEqualsRightSum {
+
+    public static void main() {
+        //1,2,3,9,4,2 and 1,2,-2
+        List<Integer> list = Arrays.asList(1,2,3,9,4,2);
+        int n = list.size();
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        int sum=0;
+        for (int i=0; i< n; i++) {
+            left[i] = sum;
+            sum += list.get(i);
+        }
+        System.out.println(Arrays.toString(left));
+
+        sum = 0;
+        for (int i=n-1; i>0; i--) {
+            right[i] = sum;
+            sum += list.get(i);
+        }
+        System.out.println(Arrays.toString(right));
+
+        for(int i=0; i< left.length; i++){
+            if( left[i] == right[i]) {
+                System.out.print("Index: "+ i);
+            }
+        }
+    }
+}
